19106aff3dc372992a200848c6042dccd1112eec
Schwinn Saereesitthipitak <secure@szawinis.com>
2020-05-20 00:46:31 +0700

    Improved zsh config compatibility with MacOS

--- .config/install_mac.sh
+++ .config/install_mac.sh
@@ -1,3 +1,5 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/tmux-plugins/tpm ~/.local/tmux-plugins/tpm
 
 # Vim
